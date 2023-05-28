924292b5f4bb94c5f90cc7c1ca4d4a63b4e6dd5a
Anders Aagaard <aagaande@gmail.com>
2023-05-28 10:50:10 +0200

    add retrying

--- shared.py
+++ shared.py
@@ -1,13 +1,46 @@
 import asyncio
-import logging
-import os
 from abc import ABC, abstractmethod
+from contextlib import asynccontextmanager
 from dataclasses import dataclass
+from functools import wraps
 from typing import List
 
 import guidance
 from telegram import Message, Update, constants
+from telegram.error import RetryAfter, TimedOut
 from telegram.ext import ContextTypes
+
+
+def retry(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        retries = 5
+        backoff = 2
+        last_exception = None
+
+        for _ in range(5):
+            retries += 1
+            try:
+                return await func(*args, **kwargs)
+            except RetryAfter as e:
+                last_exception = e
+                await asyncio.sleep(e.retry_after)
+                backoff = 1
+                continue
+            except TimedOut as e:
+                last_exception = e
+                await asyncio.sleep(backoff)
+                backoff += 2
+                continue
+            except Exception as e:
+                last_exception = e
+                await asyncio.sleep(backoff)
+                backoff += 2
+                continue
+        assert last_exception
+        raise last_exception
+
+    return wrapper
 
 
 @dataclass
@@ -30,12 +63,23 @@ class ChatContext:
 
     async def stream_chatgpt_reply(self, reply: guidance.Program):
         await self.telegram_action_typing()
-        telegram_message = await self.reply_text("...")
+
+        @retry
+        async def create_inital_message():
+            return await self.reply_text("...")
+
+        @retry
+        async def edit_message(telegram_message: Message, text: str):
+            await telegram_message.edit_text(response)
+
+        telegram_message = await create_inital_message()
+        print("Telegram message created", telegram_message)
+
         response = ""
         for running_reply in reply:
             response = running_reply.get("answer", "").strip()
             if response:
-                await telegram_message.edit_text(response)
+                await edit_message(telegram_message, response)
         return response
 
 
